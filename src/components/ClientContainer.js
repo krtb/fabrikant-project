5a0a1eca409c1e0d5f90c5206aca8be05ec730f3
Kurt Bauer <kurtbauer55@protonmail.com>
2019-05-06 14:46:22 -0400

    crete initial functional component and style sheet

---
+++ src/components/ClientContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ClientToggle from './ClientToggle';
+import ClientForm from './ClientForm'
+
+const ClientContainer = () => {
+    return(
+        <div>
+        <ClientToggle />
+        <ClientForm />
+        </div>
+    )
+}
+
+export default ClientContainer

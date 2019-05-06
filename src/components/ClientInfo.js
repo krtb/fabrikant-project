50250b93311c3362d8e1910aba7520ddf5fe8192
Kurt Bauer <kurtbauer55@protonmail.com>
2019-05-06 15:46:46 -0400

    add secondary info div and styling into clientinfo component

--- src/components/ClientInfo.js
+++ src/components/ClientInfo.js
@@ -3,8 +3,13 @@ import React from 'react';
 
 const ClientInfo = () => {
     return (
+        <div>
         <div className="client-info-primary">
             PRIMARY
+        </div>
+        <div className="client-info-secondary">
+            secondary
+        </div>
         </div>
     )
 }

2983634a13e0886b96ab610262583b8cf193cd3f
Kurt Bauer <kurtbauer55@protonmail.com>
2019-05-06 15:39:41 -0400

    add primary info div with css styling to make scrollable

---
+++ src/components/ClientInfo.js
@@ -0,0 +1,12 @@
+import './css/ClientInfo.css'
+import React from 'react';
+
+const ClientInfo = () => {
+    return (
+        <div className="client-info-primary">
+            PRIMARY
+        </div>
+    )
+}
+
+export default ClientInfo

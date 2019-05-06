57a333d9e718b85de4a98458a259b4042341a7db
Kurt Bauer <kurtbauer55@protonmail.com>
2019-05-06 14:47:00 -0400

    remove client toggle component and add in container div

--- src/components/ClientForm.js
+++ src/components/ClientForm.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ClientToggle from './ClientToggle';
 
 const ClientForm = () => {
     return(
-        <ClientToggle />
+        <div>
+            CLIENT CONTAINER
+        </div>
     )
 }
 

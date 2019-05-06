4700338eb2c4da1d2b22c9749b457477fa0a0d0b
Kurt Bauer <kurtbauer55@protonmail.com>
2019-05-06 19:54:28 -0400

    add form, label and input properties to component

--- src/components/ClientInfo.js
+++ src/components/ClientInfo.js
@@ -5,7 +5,16 @@ const ClientInfo = () => {
     return (
         <div>
         <div className="client-info-primary">
-            PRIMARY
+            <form>
+                <div className="label-div">
+                    <label>
+                        First Name:
+                    </label>
+                </div>
+                <div className="input-div">
+                    <input class="form-input" name="first name" value="" />
+                </div>
+            </form>
         </div>
         <div className="client-info-secondary">
             secondary

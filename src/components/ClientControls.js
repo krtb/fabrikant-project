e0a0632a2b52ca7239992b7761f4bd5cf56aced6
Kurt Bauer <kurtbauer55@protonmail.com>
2019-05-06 13:13:11 -0400

    add new client controls component

---
+++ src/components/ClientControls.js
@@ -0,0 +1,17 @@
+import './css/ClientControls.css'
+import React from 'react'
+
+const ClientControls = () => {
+    return(
+        <div>
+            <div>
+                ADD NEW CLIENT
+            </div>
+            <div>
+                EDIT CLIENT
+            </div>
+        </div>
+    )
+}
+
+export default ClientControls

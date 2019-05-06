3f31681fc998298ea54fd393fb8e7dca9a7c9638
Kurt Bauer <kurtbauer55@protonmail.com>
2019-05-06 15:37:31 -0400

    remove unecessary container wrapping navbar and clientcontainer

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,13 @@ import './css/App.css'
 import React from 'react';
 
 import Navbar from './Navbar'
-import ClientForm from './ClientContainer'
 import ClientContainer from './ClientContainer';
 
 const App = () => {
     return (
         <div className="main-container">
             <div className="container">
-                <div className="navbar-container">
-                    <Navbar/>
-                </div>
+                <Navbar/>
                 <ClientContainer />
             </div>
         </div>

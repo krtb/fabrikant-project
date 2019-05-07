398b32265fb0be106706ac2ec904870903c45af5
Kurt Bauer <kurtbauer55@protonmail.com>
2019-05-07 10:53:13 -0400

    add state to toggle add/edit buttons, add onClick function to set state if toggled

--- src/components/ClientContainer.js
+++ src/components/ClientContainer.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
+
 import ClientToggle from './ClientToggle';
 import ClientForm from './ClientForm'
 
-const ClientContainer = () => {
-    return(
-        <div>
-        <ClientToggle />
-        <ClientForm />
-        </div>
-    )
+export class ClientContainer extends React.Component {
+    state = {
+        viewDifferentPage: false,
+    }
+
+    changePage = () => {
+        console.log('is clicked!');
+        this.setState({
+            viewDifferentPage: !this.state.viewDifferentPage
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <ClientToggle onClick={this.changePage} />
+                <ClientForm viewDifferentPage={this.state.viewDifferentPage} />
+            </div>
+        );
+    }
 }
 
 export default ClientContainer

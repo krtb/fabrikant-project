53ca9add34f0a8eeaebc35480cd33ff00e5b736d
Kurt Bauer <kurtbauer55@protonmail.com>
2019-05-07 09:34:29 -0400

    add all primary input components

--- src/components/ClientInfo.js
+++ src/components/ClientInfo.js
@@ -6,6 +6,7 @@ const ClientInfo = () => {
         <div>
         <div className="client-info-primary">
             <form>
+                <div className="inner-form-div">
                 <div className="label-div">
                     <label>
                         First Name:
@@ -14,6 +15,68 @@ const ClientInfo = () => {
                 <div className="input-div">
                     <input class="form-input" name="first name" value="" />
                 </div>
+                </div>
+                <div className="inner-form-div">
+                    <div className="label-div">
+                        <label>
+                            Last Name:
+                </label>
+                    </div>
+                    <div className="input-div">
+                        <input class="form-input" name="last name" value="" />
+                    </div>
+                </div>
+                <div className="inner-form-div">
+                    <div className="label-div">
+                        <label>
+                            Email:
+                </label>
+                    </div>
+                    <div className="input-div">
+                        <input class="form-input" name="email" value="" />
+                    </div>
+                </div>
+                <div className="inner-form-div">
+                    <div className="label-div">
+                        <label>
+                            Phone:
+                </label>
+                    </div>
+                    <div className="input-div">
+                        <input class="form-input" name="phone" value="" />
+                    </div>
+                </div>
+                <div className="inner-form-div">
+                    <div className="label-div">
+                        <label>
+                            Instagram:
+            </label>
+                    </div>
+                    <div className="input-div">
+                        <input class="form-input" name="Instagram" value="" />
+                    </div>
+                </div>
+                <div className="inner-form-div">
+                    <div className="label-div">
+                        <label>
+                            Birthdate:
+            </label>
+                    </div>
+                    <div className="input-div">
+                        <input class="form-input" name="birthdate" value="" />
+                    </div>
+                </div>
+                <div className="inner-form-div">
+                    <div className="label-div">
+                        <label>
+                            Ringsize:
+            </label>
+                    </div>
+                    <div className="input-div">
+                        <input class="form-input" name="ringsize" value="" />
+                    </div>
+                </div>
+
             </form>
         </div>
         <div className="client-info-secondary">

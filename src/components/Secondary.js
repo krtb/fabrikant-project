54fec23c7902d371c14fa5a2ed1b6728fd7ca858
Kurt Bauer <kurtbauer55@protonmail.com>
2019-05-07 09:48:00 -0400

    add secondary window pane for client input

---
+++ src/components/Secondary.js
@@ -0,0 +1,103 @@
+import React from 'react';
+
+const Secondary = () => {
+    return(
+        <div className="client-info-secondary">
+            <form>
+                <div className="inner-form-div">
+                    <div className="label-div">
+                        <label>
+                            Significant Other:
+                    </label>
+                    </div>
+                    <div className="input-div">
+                        <input class="form-input" name="significant other" value="" />
+                    </div>
+                </div>
+                <div className="inner-form-div">
+                    <div className="label-div">
+                        <label>
+                            Significant Birthdate:
+                </label>
+                    </div>
+                    <div className="input-div">
+                        <input class="form-input" name="Significant Birthdate" value="" />
+                    </div>
+                </div>
+                <div className="inner-form-div">
+                    <div className="label-div">
+                        <label>
+                            Significant Other Email:
+                </label>
+                    </div>
+                    <div className="input-div">
+                        <input class="form-input" name="Significant Other Email" value="" />
+                    </div>
+                </div>
+                <div className="inner-form-div">
+                    <div className="label-div">
+                        <label>
+                            Significant Other Phone:
+                </label>
+                    </div>
+                    <div className="input-div">
+                        <input class="form-input" name="Significant Other Phone" value="" />
+                    </div>
+                </div>
+                <div className="inner-form-div">
+                    <div className="label-div">
+                        <label>
+                            Significant Other Instagram:
+            </label>
+                    </div>
+                    <div className="input-div">
+                        <input class="form-input" name="Significant Other Instagram" value="" />
+                    </div>
+                </div>
+                <div className="inner-form-div">
+                    <div className="label-div">
+                        <label>
+                            Special Occasion:
+            </label>
+                    </div>
+                    <div className="input-div">
+                        <input class="form-input" name="special occasion" value="" />
+                    </div>
+                </div>
+                <div className="inner-form-div">
+                    <div className="label-div">
+                        <label>
+                            Special Occasion Date:
+            </label>
+                    </div>
+                    <div className="input-div">
+                        <input class="form-input" name="Special Occasion Date" value="" />
+                    </div>
+                </div>
+                <div className="inner-form-div">
+                    <div className="label-div">
+                        <label>
+                            Significant Other Ring Size:
+            </label>
+                    </div>
+                    <div className="input-div">
+                        <input class="form-input" name="Significant Other Ring Size" value="" />
+                    </div>
+                </div>
+                <div className="inner-form-div">
+                    <div className="label-div">
+                        <label>
+                            Attended By:
+            </label>
+                    </div>
+                    <div className="input-div">
+                        <input class="form-input" name="Attended By" value="" />
+                    </div>
+                </div>
+
+            </form>
+        </div>
+    )
+}
+
+export default Secondary
